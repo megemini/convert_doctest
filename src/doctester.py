37302b3d758f10a8e14ce3c692b9bdd3c08e320c
megemini <megemini@outlook.com>
2023-09-03 11:38:13 +0800

    [Fix] _test_capacity to set

--- src/doctester.py
+++ src/doctester.py
@@ -377,7 +377,7 @@ class Xdoctester(DocTester):
         self.mode = mode
         self.verbose = verbose
         self.config = {**XDOCTEST_CONFIG, **(config or {})}
-        self._test_capacity = {}
+        self._test_capacity = set()
 
         self._patch_global_state = patch_global_state
         self._patch_tensor_place = patch_tensor_place
